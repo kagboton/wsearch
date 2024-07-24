fc073d1e276d5729d1f41791fe203b394983fbd1
Kilian Agboton <agbotonstefane@gmail.com>
2024-07-23 22:02:26 -0400

    send api response to page-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { PageListComponent } from './page-list/page-list.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { WikipediaService } from './wikipedia.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PageListComponent, SearchBarComponent],
+  imports: [RouterOutlet, PageListComponent, SearchBarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  pages = [];
+
   constructor(private wikipedia: WikipediaService) { 
   }
   
   onTerm(term: string) {
-    const results = this.wikipedia.search(term);
-    console.log('search term :', results);
+    this.wikipedia.search(term).subscribe((response: any) => { 
+      this.pages = response.query.search;
+    });
+    
   }
 }
--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './page-list.component.html',
   styleUrl: './page-list.component.css'
 })
 export class PageListComponent {
-
+  @Input() pages: any = [];
 }
